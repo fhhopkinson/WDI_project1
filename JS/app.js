350312b975bf3bcb4ec34b911626759a8364564f
fred <fhhopkinson@gmail.com>
2016-01-30 15:42:15 +0000

    just about to redo win logic so committing

--- JS/app.js
+++ JS/app.js
@@ -20,6 +20,8 @@ var lives = 7;
 var player1;
 var player2;
 
+var squirrelLetterArr = [];
+
 //Add event listener to letter tiles
 $('.tile').on("click",function() {
 
@@ -27,22 +29,17 @@ var squirrelsArray = $(".squirrel");//Put squirrels in an array
 var squirrelIndex = startArray.indexOf(this.id);//Attach desired index number to variable
 
   console.log('click');
-  console.log(startArray);
-
-
-  var letterClicked = $(this.id).val();
-var squirrelLetterArr;
-squirrelLetterArr = $("#squirrel").each(function(i, li){
-            a.push($(li))});
-  
+  console.log("this is startArray " + startArray);
+ var letterClicked = $(this.id).val();
+    
   //If letter clicked is in startArray...
   if(startArray.indexOf(this.id)!== -1) {
     console.log("The index of the letter you clicked: " + startArray.indexOf(this.id));
-    //Add HTML of clicked letter to the appropriate index of squirrel array
-    squirrelsArray[squirrelIndex].innerHTML = (this.id);
-    // $(this.id).toggle();
-console.log('is squirrelLetterArr filling' + squirrelLetterArr);
-console.log('squirrelIndex' + squirrelIndex);
+    squirrelsArray[squirrelIndex].innerHTML = (this.id);//Add HTML of clicked letter to the appropriate index of squirrel array
+    squirrelLetterArr[startArray.indexOf(this.id)] = this.id; //Push letter of button pressed to squirrelLetterArr
+
+  console.log('is squirrelLetterArr filling: ' + squirrelLetterArr);
+  console.log('squirrelIndex' + squirrelIndex);
   }else {
     lives-=1;
     console.log("number of lives" + lives);
@@ -50,7 +47,6 @@ console.log('squirrelIndex' + squirrelIndex);
      console.log('player1 wins');
      }else if(startArray == squirrelLetterArr){
         console.log('player2 wins');
-        console.log(squirrelLetterArr);
      } else {
         console.log("else at the end of the win logic" + this.id);
      }
@@ -58,16 +54,7 @@ console.log('squirrelIndex' + squirrelIndex);
 });
 });
 
-// var endArray = endWord.split("");
-// var endWord = endArray.toString()
-
-
-// startArray.indexOf()
-
-// $(#tile.id).on("click",function() {
-//   if(content of the indices of startArray == this.id)
-//    content of the index of startArray.text into box of correct index
-//   else{
-// (#tile.id).text("");
 
 //make space bar populate a color.
+
+// $(this.id).toggle();
