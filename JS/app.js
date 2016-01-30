1c9db33da1ccbe3de7acbf5bc5c6f84a93262b47
fred <fhhopkinson@gmail.com>
2016-01-30 12:28:08 +0000

    inputting letters in to the board which is aawesome but there are a few issues. I am tidying up so may mess it up

---
+++ JS/app.js
@@ -0,0 +1,77 @@
+$(function(){
+
+
+  //Placeholder Array for testing purposes
+  var startArray = ["A", "b", "c"];
+  console.log(startArray);
+
+  $("form").on("submit", function(e){
+    e.preventDefault();
+    var startWord = $("input#startWord").val();
+
+    startArray = startWord.split("");
+    console.log(startArray);
+
+  });
+
+
+
+
+//win logic
+var lives = 7;
+var player1;
+var player2;
+
+var letterClicked = "";
+
+//Put squirrels in an array
+var squirrelsArray = $(".squirrel");
+
+//Add event listener to letter tiles
+$('.tile').on("click",function() {
+  console.log('click');
+  endArray = $("input#tile").push();
+  console.log(startArray);
+
+  //If letter clicked is in startArray...
+  if(startArray.indexOf(this.id)!== -1) {
+
+    // (this.id).text(('#squirrel').indexOf(startArray));
+    console.log("The index of the letter you clicked: " + startArray.indexOf(this.id));
+
+    //Attach desired index number to variable
+    var squirrelIndex = startArray.indexOf(this.id);
+
+    //Add HTML of clicked letter to the appropriate index of squirrel array
+    squirrelsArray[squirrelIndex].innerHTML = (this.id);
+    // $(this.id).toggle();
+console.log('is squirrelsArray filling' + squirrelsArray);
+console.log('squirrelIndex' + squirrelIndex);
+  }else {
+    lives-=1;
+    console.log("number of lives" + lives);
+    if (lives === 0) {
+     console.log('player1 wins');
+     }else if(startArray == squirrelsArray){
+        console.log('player2 wins');
+        console.log(squirrelsArray);
+     } else {
+        console.log("else at the end of the win logic" + this.id);
+     }
+  };
+});
+});
+
+// var endArray = endWord.split("");
+// var endWord = endArray.toString()
+
+
+// startArray.indexOf()
+
+// $(#tile.id).on("click",function() {
+//   if(content of the indices of startArray == this.id)
+//    content of the index of startArray.text into box of correct index
+//   else{
+// (#tile.id).text("");
+
+//make space bar populate a color.
---
+++ JS/app2.js
@@ -0,0 +1,34 @@
+/*$(function(){
+}
+
+
+//Minimum Viable Product
+
+2 player
+enter a word
+guess a word
+win logic
+Basic CSS
+use jQuery
+Bucket List
+
+Score Best of 5 
+
+Hangman picture/animation
+sound effects
+timer
+CSS fest
+
+
+Intital stuff to get to grips with:
+
+
+when the correct letter is selected from the buttons displaying the letters on the on click if the letter matches any in the array the letter is revealed. If the letter is not in the arr then the tile is hidden.(the amount of letters can be recorded for the hanging feature.) on click hide every tile of the alphabet.
+
+win logic is created by both arrays matching each other and then  saying that player two wins. Player one wins when all the letters of the alphabet are exhausted.
+
+CSS will require 26 tiles at least.  Score counter? pop up startWord. anitmation of the hanging in 7 moves.
+
+
+considerations: $("#score1").text(player1Score)
+                $("#score2").text(Player2Score)*/
