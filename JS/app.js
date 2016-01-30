5147b87f19ad13e96230ba5116a3b56f7f6ad50b
fred <fhhopkinson@gmail.com>
2016-01-30 13:50:32 +0000

    going for li=uncha nd trying to record whatever progress i have made

--- JS/app.js
+++ JS/app.js
@@ -2,7 +2,7 @@ $(function(){
 
 
   //Placeholder Array for testing purposes
-  var startArray = [];
+  var startArray;
   console.log(startArray);
 
   $("form").on("submit", function(e){
@@ -10,54 +10,47 @@ $(function(){
     var nextWord;
     var startWord = $("input#startWord").val();
     nextWord = startWord.toUpperCase(this.id);
-
-    
-    console.log(nextWord)
     startArray = nextWord.split("");
+    console.log(nextWord)
     console.log(startArray);
     
   });
-
-
-
-
 //win logic
 var lives = 7;
 var player1;
 var player2;
 
-var letterClicked = "";
-
-//Put squirrels in an array
-var squirrelsArray = $(".squirrel");
-
 //Add event listener to letter tiles
 $('.tile').on("click",function() {
+
+var squirrelsArray = $(".squirrel");//Put squirrels in an array
+var squirrelIndex = startArray.indexOf(this.id);//Attach desired index number to variable
+
   console.log('click');
   console.log(startArray);
 
+
+  var letterClicked = $(this.id).val();
+var squirrelLetterArr;
+squirrelLetterArr = $("#squirrel").each(function(i, li){
+            a.push($(li))});
+  
   //If letter clicked is in startArray...
   if(startArray.indexOf(this.id)!== -1) {
-
-    // (this.id).text(('#squirrel').indexOf(startArray));
     console.log("The index of the letter you clicked: " + startArray.indexOf(this.id));
-
-    //Attach desired index number to variable
-    var squirrelIndex = startArray.indexOf(this.id);
-
     //Add HTML of clicked letter to the appropriate index of squirrel array
     squirrelsArray[squirrelIndex].innerHTML = (this.id);
     // $(this.id).toggle();
-console.log('is squirrelsArray filling' + squirrelsArray);
+console.log('is squirrelLetterArr filling' + squirrelLetterArr);
 console.log('squirrelIndex' + squirrelIndex);
   }else {
     lives-=1;
     console.log("number of lives" + lives);
     if (lives === 0) {
      console.log('player1 wins');
-     }else if(startArray == squirrelsArray){
+     }else if(startArray == squirrelLetterArr){
         console.log('player2 wins');
-        console.log(squirrelsArray);
+        console.log(squirrelLetterArr);
      } else {
         console.log("else at the end of the win logic" + this.id);
      }
