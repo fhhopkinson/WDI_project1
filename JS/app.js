c3b79f73aed8b26e62b7ab6e3ff552122e8a64cf
fred <fhhopkinson@gmail.com>
2016-02-01 14:02:02 +0000

    commiting before wrapping in functions both the image and the life counter.

--- JS/app.js
+++ JS/app.js
@@ -1,29 +1,17 @@
 $(function(){
 
-
-  //Placeholder Array for testing purposes
   var startArray;
   console.log(startArray);
-  $("form").on("submit", function(e){
+  $("form").on("submit", function(e){ //have to prevent default as the page refreshes after pressing enter.
     e.preventDefault();
     var nextWord;
     var startWord = $("input#startWord").val();
-    startWord = startWord.split(" ").join("_");
-    nextWord = startWord.toUpperCase(this.id);
-    startArray = nextWord.split("");
+    startWord = startWord.split(" ").join("_");// turning a space into an underscore in the array
+    nextWord = startWord.toUpperCase(this.id); //to match the buttons on click case
+    startArray = nextWord.split(""); //spltiing the string into individual letters per index
 
-    console.log(nextWord)
-    console.log(startArray);
-    console.log(startArray.length);
-
-    $.each(startArray, function(index, value) {
-    //(startArray.replace(" ", "_")) // run a loop over the array and this fills the unordered list with the required number of answerTile for the game.
-    // $(document).keydown(function(e) { 
-    // if (startArray.replace(" ", "_")) { 
-    //   ( $(".answerTiles").append('<li class="answerTile">_</li>'))
-      console.log("testing whether appending tile to html " + ".answerTile");
+    $.each(startArray, function(index, value) { // add the list to the ul so that the letters go into new boxes
       ($(".answerTiles").append('<li class="answerTile"></li>'));
-
     });
   });
 //win logic
@@ -36,8 +24,6 @@ $(function(){
     var letterIndex = startArray.indexOf(this.id);//Attach desired index number to variable
     var self = this; // store this as self so we can use it inside the forEach loop.
     var match = false;
-    console.log('click');
-    console.log("this is startArray " + startArray);
     
     startArray.forEach(function(letter, index) { //here we have changed if to self because otherwise it would refer to .tile rather than startArray.
       if(letter === self.id) {
@@ -46,28 +32,28 @@ $(function(){
         match = true;
       }
     });
-    if(!match && lives >= 1) { //lives diminishing logic
+
+
+    if(!match && lives >= 1) { //lives diminishing logic ie losing lives. if match is false lose a life
       lives-=1;
-      $('#lives').text(lives);
+      $('#lives').text(lives);// adding number of lives to text box
       console.log("number of lives " + lives);
     }
     if (lives == 0) {
-      console.log('player1 wins');
-      $('#winner').text("Player 1");
-    } else if(startArray.toString() === correctLettersArr.toString()) {
-      console.log('player2 wins');
-      $('#winner').text("Player 2");
+      $('#winner').text("Butcher");
+    } else if(startArray.toString() === correctLettersArr.toString()) { //tried doing this and it worked so i kept it
+      $('#winner').text("Innocent");
     } else {
-      console.log("else at the end of the win logic " + this.id);
     } 
-    if (lives === 9) { //logic for image changing from nothing to hanging
+    
+
+    if (lives === 9) { //logic for image changing from good luck to hanging
     $(".hang10").css({"background-image": "url('images/IMG_1.JPG')"});
     console.log("lives");
     } else if (lives === 8) {
     $(".hang10").css({"background-image": "url('images/IMG_2.JPG')"});
     } else if (lives === 7) {
     $(".hang10").css({"background-image": "url('images/IMG_3.JPG')"});
-    console.log('lives2');
     } else if (lives === 6) {
     $(".hang10").css({"background-image": "url('images/IMG_4.JPG')"});
     } else if (lives === 5) {
@@ -81,12 +67,18 @@ $(function(){
     } else if (lives === 1) {
     $(".hang10").css({"background-image": "url('images/IMG_9.JPG')"});
     } else if (lives === 0) {
-    $(".hang10").css({"background-image": "url('images/IMG_10.JPG')"});
+    $(".hang10").css({"background-image": "url('images/IMG_11.JPG')"});
     }
+
+
   });
+
   $("#refresh").on('click', function () { //refresh button logic
    location.reload(true);
-   console.log('reset');
   });
 });
+
+function checkWinner() {}
+function changePicture() {}
+
 
